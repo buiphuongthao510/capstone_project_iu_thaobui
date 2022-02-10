0cc9d754f720a2fa6389260d6bcd569161c71e48
Bui, Thao Phuong <thaobui@iu.edu>
2022-02-10 12:00:08 -0500

    test get ticket

--- front/front/4/login.php
+++ front/front/4/login.php
@@ -2,4 +2,10 @@
 if (!$authenticated) { $_SESSION['CAS']= true;
     header("Location:https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.html");
 }
+if ($authenticated) {      
+    //validate since authenticated   
+    if (isset($_GET["ticket"])) {
+         echo $_GET["ticket"];
+    }
+}
 ?>

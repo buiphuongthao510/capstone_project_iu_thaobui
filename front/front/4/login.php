4f0f696376db87b7101736b761f0da703de8b32d
Bui, Thao Phuong <thaobui@iu.edu>
2022-02-10 11:52:29 -0500

    login test

--- front/front/4/login.php
+++ front/front/4/login.php
@@ -1,5 +1,5 @@
 <?php
 if (!$authenticated) { $_SESSION['CAS']= true;
-    header("Location:https://https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.html");
+    header("Location:https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.html");
 }
 ?>

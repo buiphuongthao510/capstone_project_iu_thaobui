babaa812072c2acfbb7bd56b93ef522d198368b0
Bui, Thao Phuong <thaobui@iu.edu>
2022-02-15 15:49:38 -0500

    cas login fix

--- front/front/4/index.php
+++ front/front/4/index.php
@@ -219,10 +219,14 @@
     <?php
     // if ($authenticated) {      
     //validate since authenticated   
-    if (isset($_GET["ticket"])) {
-         echo $_GET["ticket"];
+    if ($authenticated) {      
+      //validate since authenticated   
+      if (isset($_GET["ticket"])) {
+          header("Location: https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=$_GET[ticket]&service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
       }
-    // }
+    }
+    $username = $_SESSION['user'];
+    echo $username;
     ?>
   </body>
 </html>
--- front/front/4/login.php
+++ front/front/4/login.php
@@ -1,11 +1,15 @@
 <?php
+session_start();
 if (!$authenticated) { $_SESSION['CAS']= true;
     header("Location:https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
 }
-if ($authenticated) {      
-    //validate since authenticated   
-    if (isset($_GET["ticket"])) {
-        header("Location: https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=$_GET[ticket]&service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
-    }
-}
+// if ($authenticated) {      
+//     //validate since authenticated   
+//     if (isset($_GET["ticket"])) {
+//         header("Location: https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=$_GET[ticket]&service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
+//     }
+// }
+// $username = $_SESSION['user'];
+
+// echo $username;
 ?>

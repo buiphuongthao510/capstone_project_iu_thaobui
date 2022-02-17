20a9b39deaf29077bf975bebb0596541f061a43b
zz13 <zz13@iu.edu>
2022-02-17 15:08:21 -0500

    Test

--- front/front/4/index.php
+++ front/front/4/index.php
@@ -1,3 +1,19 @@
+<?php
+
+$servername = "db.luddy.indiana.edu";
+$username = "i494f21_team21";
+$password = "my+sql=i494f21_team21";
+$dbname = "i494f21_team21";
+
+// Create connection
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " .$conn->connect_error);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -29,15 +45,19 @@
     
     <a href="profile.php">TEST</a>
     <div class="banner">
-      <div class="text">xxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
-      <div class="button">Join Today</div>
+      <div class="text">Get Involved</div>
+      
     </div>
 
     <section>
       <div class="title">Find your passion group</div>
       <div class="group-list">
         <a class="group-item">
-          <div>#####</div>
+          <div>
+            <?php
+              $sql = 'SELECT COUNT(o_id);';
+            ?>
+          </div>
           <div>Student</div>
           <div>organizations</div>
         </a>

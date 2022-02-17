59f59750b75ed15fd28cb94ad1f389be77e54353
buithao <thaobui@iu.edu>
2022-02-17 13:30:25 -0500

    cas login ticket echo

--- front/front/4/index.php
+++ front/front/4/index.php
@@ -222,7 +222,8 @@
     if ($authenticated) {      
       //validate since authenticated   
       if (isset($_GET["ticket"])) {
-          header("Location: https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=$_GET[ticket]&service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
+          echo $_GET["ticket"];
+          // header("Location: https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=.$_GET[ticket].&service=https://cgi.luddy.indiana.edu/~team21/front/front/4/index.php");
       }
     }
     $username = $_SESSION['user'];

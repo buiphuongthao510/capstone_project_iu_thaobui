0d3a95d45ba40d663e47e023155b89b34719afe4
buithao <thaobui@iu.edu>
2022-02-22 17:35:29 -0500

    update style.css file path and change title

--- login.php
+++ login.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="./style.css" rel="stylesheet" />
-    <title>Document</title>
+    <link href="/index/style.css" rel="stylesheet" />
+    <title>Youthon</title>
   </head>
 
   <body>

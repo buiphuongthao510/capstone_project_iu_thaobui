fba5df0c828d88da3ecf8783f81e50881fabd321
buithao <thaobui@iu.edu>
2022-02-24 17:46:18 -0500

    data display fix

--- index/profile.php
+++ index/profile.php
@@ -61,40 +61,43 @@
             $select = mysqli_query($conn, $sql_select);
 
             // display contents 
-            if (!$select) {
-                echo $sql_select;
-                echo $conn;
-            } else {
-                while ($data = mysqli_fetch_assoc($select)) {
-                        echo "<div>First name: {$data['first_name']}</div>";
-                        echo "<div>Last name: {$data['last_name']}</div>";
-                        echo "<div>Date of Birth: {$data['dob']}</div>";
-                        echo "<div>Email: {$data['email']}</div>";
+            // if (!$select) {
+            //     echo $sql_select;
+            //     echo $conn;
+            // } else {
+            //     while ($data = mysqli_fetch_assoc($select)) {
+            //             echo "<div>First name: {$data['first_name']}</div>";
+            //             echo "<div>Last name: {$data['last_name']}</div>";
+            //             echo "<div>Date of Birth: {$data['dob']}</div>";
+            //             echo "<div>Email: {$data['email']}</div>";
                     
-                }
-            }
+            //     }
+            
+
         ?>
       <div class="right">
         <div class="title gi">General Information</div>
         <div class="line">
+          <?php while ($data = mysqli_fetch_assoc($select)) {?>
           <div class="form-item">
-            <div>First name: </div>
+            <div>First name:<?php echo $data['first_name']; ?> </div>
             <!-- <input type="text" placeholder="Sam" /> -->
           </div>
           <div class="form-item">
-            <div>Last name: </div>
+            <div>Last name: <?php echo $data['last_name']; ?></div>
             <!-- <input type="text" placeholder="Fischer" /> -->
           </div>
         </div>
         <div class="line">
           <div class="form-item">
-            <div>Date of Birth: </div>
+            <div>Date of Birth: <?php echo $data['dob']; ?></div>
             <!-- <input type="text" placeholder="mm/dd/yyyy" /> -->
           </div>
         </div>
         <div class="line">
           <div class="form-item">
-            <div>Email: </div>
+            <div>Email: <?php echo $data['email'];?> </div>
+            <?php }?>
             <!-- <input type="text" placeholder="sfsich@email.com" /> -->
           </div>
         </div>

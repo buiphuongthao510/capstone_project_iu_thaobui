6c451de191e1cc87d8040aaffc5b28c3770bad50
buithao <thaobui@iu.edu>
2022-02-24 17:38:39 -0500

    add database connection to display profile data

--- index/profile.php
+++ index/profile.php
@@ -39,7 +39,18 @@
         </div>
       </div>
       <?php 
-            include './includes/dbConnect.php';
+            $servername = "db.luddy.indiana.edu";
+            $username = "i494f21_team21";
+            $password = "my+sql=i494f21_team21";
+            $dbname = "i494f21_team21";
+            
+            // Create connection
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+            }
             session_start();
 
             $username = $_SESSION["username"];

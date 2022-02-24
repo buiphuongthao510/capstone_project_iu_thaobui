d39f3baf42bd4da6d6db553e6babde6e9f6f1764
zz13 <zz13@iu.edu>
2022-02-24 14:05:21 -0500

    Test data connection

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -1,5 +1,7 @@
 <?php
+function $OpenCon() 
 
+{
 $servername = "db.luddy.indiana.edu";
 $username = "i494f21_team21";
 $password = "my+sql=i494f21_team21";
@@ -7,6 +9,9 @@ $dbname = "i494f21_team21";
 
 // Create connection
 $conn = mysqli_connect($servername,$username,$password,$dbname);
+return $conn
+}
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " .$conn->connect_error);

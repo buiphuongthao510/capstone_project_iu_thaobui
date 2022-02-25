fefbb7ded15589a25e60bdcfab6a6b296aafee70
ykien <ykien@iu.edu>
2022-02-25 00:01:47 -0500

    test insert database

--- events/submitEvent.php
+++ events/submitEvent.php
@@ -26,6 +26,8 @@
 		$event_date = $_REQUEST['event_date'];
 		$address = $_REQUEST['address'];
 
+	//AUTO_INCREMENT o_id
+		$sql = "ALTER TABLE events MODIFY o_id int NOT NULL AUTO_INCREMENT";
 	// Insert Query 
 		$sql = "INSERT INTO events(event_name,event_date,address) VALUES ('".$event_name."','".$event_date."','".$address."')";
 

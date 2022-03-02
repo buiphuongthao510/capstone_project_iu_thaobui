0c162a0934e9a4a433f93ed4423245f105bbb3fc
ykien <ykien@iu.edu>
2022-03-01 23:16:39 -0500

    test data display

--- events/events.php
+++ events/events.php
@@ -126,7 +126,7 @@
 			      $o_id = $_SESSION["o_id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE o_id = "NULL" ;";
+			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE o_id = 'NULL' ;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

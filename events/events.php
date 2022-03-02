1d6d2be52a9c3bb11cebabffa06e404f7626a6e8
ykien <ykien@iu.edu>
2022-03-01 23:19:39 -0500

    test data display

--- events/events.php
+++ events/events.php
@@ -126,7 +126,7 @@
 			      $id = $_SESSION["id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE id = int() ;";
+			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE id = 1 ;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

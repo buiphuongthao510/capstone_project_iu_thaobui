32c4d3be092c41406749d8170f1f63bbb87e1ce7
Z01Y <zhang.ziyue99@gmail.com>
2022-03-04 18:49:14 -0500

    update style sheet for organizations page

--- organizations/organizationsPage.php
+++ organizations/organizationsPage.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href=".events/styleEvent.css" rel="stylesheet">
-  <title>Document</title>
+  <link href="./styleOrganizations.css" rel="stylesheet">
+  <title>Organization Page</title>
 </head>
 
 <?php include './includes/header.php'; ?>

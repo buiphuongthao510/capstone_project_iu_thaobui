91bee81f1d97dcff9520dd0d16b5ae54e2e52275
ykien <ykien@iu.edu>
2022-03-03 21:14:18 -0500

    test description display

--- events/reserveEvent.php
+++ events/reserveEvent.php
@@ -93,7 +93,7 @@
 			  <label><b>Location:</b> <?php echo $data['address']; ?> </label>
       <br />
       <br />
-      <label><b>Event Description:</b> <br /><textarea rows="10" cols="50"><?php echo $data['description']; ?></textarea> </label>
+      <label><b>Event Description:</b> <?php echo $data['description']; ?></label>
         <?php }?>
       
       </fieldset>

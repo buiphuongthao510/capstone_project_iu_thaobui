bf1648f2b6ad00700c39270c7b6856ef5cf9ba0c
buithao <thaobui@iu.edu>
2022-03-04 17:09:24 -0500

    logo link fix

--- index/login.php
+++ index/login.php
@@ -10,7 +10,7 @@
 
   <body>
     <header>
-      <div class="logo">Youthon</div>
+      <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
       <div class="menu-list">
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
         <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>

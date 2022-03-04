1d351f30b46d0cc588c2918f1c54b48958f6f8de
ykien <ykien@iu.edu>
2022-03-03 21:07:00 -0500

    test description display

--- events/reserveEvent.php
+++ events/reserveEvent.php
@@ -68,7 +68,7 @@
 			      $id = $_SESSION["id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE id = 1 ;";
+			$sql_select = "SELECT event_name,event_date,event_time,address,description FROM events WHERE id = 1 ;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		
@@ -91,7 +91,10 @@
         <!-- <input type="text" placeholder="hh:mm:ss" /> -->
       <br />
 			  <label><b>Location:</b> <?php echo $data['address']; ?> </label>
+      <br />
+      <label><b>Event Description:</b> <br /><textarea name="description" rows="10" cols="50"><?php echo $data['description']; ?></textarea> </label>
         <?php }?>
+      
       </fieldset>
         <!-- <input type="text" placeholder="Wilkie Auditorium" /> -->
 				

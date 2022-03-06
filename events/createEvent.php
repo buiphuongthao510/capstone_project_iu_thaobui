d9bae06da92a5dd7171cc0c3aef09d85798c97f9
Chen, Yen-Chun <chenyenc@iu.edu>
2022-03-06 17:35:55 -0500

    change horizontal navbar

--- events/createEvent.php
+++ events/createEvent.php
@@ -10,12 +10,13 @@
 
   <body>
   <header>
-      <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
-      <div class="menu-list">
-        <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
-        <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
-        <a href="">Donation</a>
-        <a href="">Search</a>
+    <ul>
+      <li><a class="logo" href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></li>
+      <li><a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a></li>
+      <li><a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a></li>
+      <li><a href="#Donation">Donation</a></li>
+      <li><a href="#Donation">Search</a></li>
+    </ul>
       </div>
       <div class="button-wrap">
         <div class="button login">Log in</div>

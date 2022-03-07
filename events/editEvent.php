42a16d2f32164a2e69cfbf85dbf0a637cf9e6b8d
Z01Y <zhang.ziyue99@gmail.com>
2022-03-07 18:24:48 -0500

    update nav bar

--- events/editEvent.php
+++ events/editEvent.php
@@ -54,6 +54,9 @@
         <div class="nav-item">
           <div><a href = "https://cgi.luddy.indiana.edu/~team21/index/profile.php"> Back </a></div>
         </div>
+        <div class="nav-item">
+          <div><a href = "https://cgi.luddy.indiana.edu/~team21/index/profile.php"> Back </a></div>
+        </div>
 		</div>
 		
 		<?php

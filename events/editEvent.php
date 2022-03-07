e2eab897a0010f3a4051d2ce760e01a13b6e6c77
chenyenc <chenyenc@iu.edu>
2022-03-07 18:06:51 -0500

    change typo and hyperlink

--- events/editEvent.php
+++ events/editEvent.php
@@ -51,9 +51,6 @@
         <div class="nav-item">
           <div><a href="https://cgi.luddy.indiana.edu/~team21/events/editEvent.php">Edit Event</a></div>
         </div>
-        <div class="nav-item">
-          <div><a href = "https://cgi.luddy.indiana.edu/~team21/index/profile.php"> Back </a></div>
-        </div>
 		</div>
 		
 		<?php
@@ -115,8 +112,8 @@
 
     <footer>
       <div class="link-wrap">
-        <a>Resource</a>
-        <a>Help deak</a>
+        <a>Resources</a>
+        <a>Help desk</a>
         <a>Report</a>
         <a>Q&A</a>
       </div>

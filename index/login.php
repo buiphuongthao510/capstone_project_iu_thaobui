09d5f64cea8ccbc5bf1290b44dacc2deb1386c63
buithao <thaobui@iu.edu>
2022-03-07 17:05:05 -0500

    login page layout fix

--- index/login.php
+++ index/login.php
@@ -1,3 +1,10 @@
+<?php
+
+include './includes/dbConnect.php';
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="./style.css" rel="stylesheet" />
-    <title>Youthon</title>
+    <title>Youthon: Home Page</title>
   </head>
 
   <body>
@@ -19,13 +26,12 @@
       </div>
       <div class="button-wrap">
         <div class="button login">
-          <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team21/index/index.php">Login</a>
+          <a href="profile.php"><button>User Profile</button></a>
         </div>
-        <div class="button">Register</div>
       </div>
     </header>
     <div class="banner">
-      <div class="text">xxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+      <div class="text">Welcome to Youthon</div>
       <div class="button">Join Today</div>
     </div>
 
@@ -33,17 +39,50 @@
       <div class="title">Find your passion group</div>
       <div class="group-list">
         <a class="group-item">
-          <div>#####</div>
+          
+            <?php
+              // CONNECT DATABASE
+              $servername = "db.luddy.indiana.edu";
+              $username = "i494f21_team21";
+              $password = "my+sql=i494f21_team21";
+              $dbname = "i494f21_team21";
+
+              // Create connection
+              $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+              // Check connection
+              if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+              }
+            
+
+            
+              // Query
+              $sql = "SELECT COUNT(*) as 'count' FROM organizations;";
+              $result = mysqli_query($conn, $sql);
+
+              if(!$result) {
+                echo $sql;
+              } else{
+                  while($data = mysqli_fetch_assoc($result)) {
+            // display content
+                    echo '<div>'.$data['count'].'</div>';  
+                  }
+                }
+            ?>  
+
+          
+          
           <div>Student</div>
           <div>organizations</div>
         </a>
         <a class="group-item">
-          <div>#####</div>
+          <div>0</div>
           <div>Transactions</div>
           <div>made</div>
         </a>
         <a class="group-item">
-          <div>#####</div>
+          <div>0</div>
           <div>Funding</div>
           <div>raised</div>
         </a>
@@ -55,7 +94,6 @@
       <div>
         <div class="title">Club of the day</div>
         <div class="text">
-          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
         </div>
       </div>
     </section>
@@ -65,30 +103,120 @@
       <div class="events-list">
         <div class="events-item">
           <img src="./img/banner2.png" />
-          <div class="name">Event name</div>
+          <div class="name">
+            <?php
+
+              // CONNECT DATABASE
+              $servername = "db.luddy.indiana.edu";
+              $username = "i494f21_team21";
+              $password = "my+sql=i494f21_team21";
+              $dbname = "i494f21_team21";
+
+              // Create connection
+              $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+              // Check connection
+              if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+              }
+            
+              // Query
+              $sql = 'SELECT event_name as name FROM events ORDER BY RAND() LIMIT 1;';
+              $result1 = mysqli_query($conn, $sql);
+              
+              if(!$result1) {
+                echo $sql;
+              } else{
+                  while($data = mysqli_fetch_assoc($result1)) {
+            // display content
+                    echo '<div>'.$data['name'].'</div>';  
+                  }
+                }
+            ?>  
+        
+          </div>
           <div class="text">
-            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
           </div>
         </div>
         <div class="events-item">
           <img src="./img/banner2.png" />
-          <div class="name">Event name</div>
+          <div class="name">
+          <?php
+
+           // CONNECT DATABASE
+           $servername = "db.luddy.indiana.edu";
+           $username = "i494f21_team21";
+           $password = "my+sql=i494f21_team21";
+           $dbname = "i494f21_team21";
+
+           // Create connection
+           $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+           // Check connection
+           if ($conn->connect_error) {
+             die("Connection failed: " .$conn->connect_error);
+           }
+          
+          //query
+          $sql = 'SELECT event_name as name FROM events ORDER BY RAND() LIMIT 1;';
+          $result2 = mysqli_query($conn, $sql);
+
+          if(!$result2) {
+            echo $sql;
+          } else{
+              while($data = mysqli_fetch_assoc($result2)) {
+        // display content
+                echo '<div>'.$data['name'].'</div>';  
+              }
+            }
+        ?>  
+    
+          </div>
           <div class="text">
-            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
           </div>
         </div>
         <div class="events-item">
           <img src="./img/banner2.png" />
-          <div class="name">Event name</div>
+          <div class="name">
+            <?php
+            // CONNECT DATABASE
+           $servername = "db.luddy.indiana.edu";
+           $username = "i494f21_team21";
+           $password = "my+sql=i494f21_team21";
+           $dbname = "i494f21_team21";
+
+           // Create connection
+           $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+           // Check connection
+           if ($conn->connect_error) {
+             die("Connection failed: " .$conn->connect_error);
+           }
+          
+           //Query
+              $sql = 'SELECT event_name as name FROM events ORDER BY RAND() LIMIT 1;';
+
+              $result3 = mysqli_query($conn, $sql);
+
+              if(!$result3) {
+                echo $sql;
+              } else{
+                  while($data = mysqli_fetch_assoc($result3)) {
+            // display content
+                    echo '<div>'.$data['name'].'</div>';  
+                  }
+                }
+            ?>  
+
+          </div>
           <div class="text">
-            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
           </div>
         </div>
       </div>
     </section>
 
     <div style="text-align: center">
-      <div class="button more">Explore more events</div>
+      <div class="button more"><a href = 'events/events.php'>Explore more events</a></div>
     </div>
 
     <section class="rank">

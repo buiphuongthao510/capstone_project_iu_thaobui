ccfa3f1d698874c5f9dd59098c92100b90e63333
Bui, Thao Phuong <thaobui@iu.edu>
2022-03-23 15:31:20 -0400

    add alert message if not logged in

--- index/index.php
+++ index/index.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
     if(!isset($_SESSION['username'])){
-      header('Location: https://cgi.luddy.indiana.edu/~team21/index/login.php');
       echo '<script type="text/javascript">';
       echo  ' alert("Unauthorized page! Please login first!")';
       echo '</script>';
+      header('Location: https://cgi.luddy.indiana.edu/~team21/index/login.php');
     }
 ?>
 

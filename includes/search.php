cacdbbcf07ec5e0b6bd4ed19175f4c0508aeea51
Chen, Yen-Chun <chenyenc@iu.edu>
2022-03-28 16:23:12 -0400

    add search.php

---
+++ includes/search.php
@@ -0,0 +1,8 @@
+
+<span>
+<input type="text" name="kw" style="border:1px solid gray" placeholder="search" onkeypress="if(event.keyCode==13){alert('searching...')}">
+<div style="position:absolute;z-index:2;border:1px solid gray;display:none">1111<br>2222<br></div>
+</span>
+<script>
+
+</script>

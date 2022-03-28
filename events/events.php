14b0b0c04952825db65b4aaf651176a001a225e5
Chen, Yen-Chun <chenyenc@iu.edu>
2022-03-28 18:51:00 -0400

    update navbar

--- events/events.php
+++ events/events.php
@@ -18,17 +18,20 @@
 <?php include './includes/header.php'; ?>
 
 <body>
-  <header>
-    <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
+    <header>
+      <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
       <div class="menu-list">
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
         <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
         <a href="">Donation</a>
         <a href="#"><?php include_once("../includes/search.php")?></a>
-        <!-- <p style="flex: 1 1 auto;">Search</p> -->
       </div>
-    <div class="ui"><a href="https://cgi.luddy.indiana.edu/~team21/index/profile.php">UI</a></div>
-  </header>
+      <div class="button-wrap">
+        <div class="button login">
+          <a href="profile.php"><button>User Profile</button></a>
+        </div>
+      </div>
+    </header>
 
   <div class="main">
     <div class="left">

2a22e290b60b50c96bc44eecb6c177d87b6e5247
Chen, Yen-Chun <chenyenc@iu.edu>
2022-03-29 13:51:41 -0400

    update events.php

--- events/events.php
+++ events/events.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./styleEvent.css" rel="stylesheet">
-    <link href="./style.css" rel="stylesheet" />
+    <link href="https://cgi.luddy.indiana.edu/~team21/index/style.css" rel="stylesheet" />
 
     <title>Events</title>
     <style>

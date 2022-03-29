5350e211773ae2b12883c42694560fc1c3dc9588
Chen, Yen-Chun <chenyenc@iu.edu>
2022-03-29 12:17:27 -0400

    add excel.php

---
+++ includes/excel.php
@@ -0,0 +1,46 @@
+<?php
+
+//must have, for export excel;
+//set code
+header('Content-type: text/html; charset=utf-8');
+//set Excel
+header("Content-type:application/vnd.ms-excel");
+//set Excel filename
+header("Content-Disposition:filename=PHP_est.xls");
+
+$conn = mysqli_connect("db.luddy.indiana.edu", "i494f21_team21", "my+sql=i494f21_team21", "i494f21_team21");
+//ditermine data work or not
+if (mysqli_connect_error($conn)) {
+die("connection failed :".mysqli_connect_error());
+}
+
+//get data from sql database, set code 
+mysqli_query($conn, "SET NAMES utf8");
+
+$sql = "SELECT first_name,last_name,email FROM members";
+//$id = $_GET['id'];
+//$sql = "SELECT first_name,last_name,email FROM members WHERE id=$id";
+
+$result = $conn->query($sql);
+//add header in excel
+$filename = array("first name","last name","email");
+foreach ($filename as $key => $value) {
+//make sure excel has no error
+$name = mb_convert_encoding($value, "gb2312" , "utf-8" );
+echo $name."\t";
+}
+//Excel next row
+echo "\n";
+
+if ($result->num_rows > 0) {
+//related sets of data
+while($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
+// save data
+// $dataArr[] = $row;
+echo $row[0]."\t".$row[1]."\t".$row[2]."\t".$row[3]."\t\n";
+}
+}else{
+echo "No query data!";
+}
+
+$conn->close();

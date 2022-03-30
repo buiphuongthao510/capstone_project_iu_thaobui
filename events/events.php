ed3ab6311f5a591eeaf3b8a1780b9cf3acf2d62f
ykien <ykien@iu.edu>
2022-03-30 19:45:06 -0400

    test data display

--- events/events.php
+++ events/events.php
@@ -137,10 +137,10 @@
             }
             session_start();
 			
-			      $id = $_SESSION["id"];
+			      $id = $_GET["id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address FROM events ORDER BY id;";
+			$sql_select = "SELECT event_name,event_date,event_time,address FROM events WHERE id=$id;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

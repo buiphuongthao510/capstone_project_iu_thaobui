0a29449a2e8d9481140d047794bfd9cd0bcb3ec4
ykien <ykien@iu.edu>
2022-03-31 04:32:03 -0400

    test data display

--- events/events.php
+++ events/events.php
@@ -162,7 +162,7 @@
       <br />
       <br />
       <br />
-      <input type="hidden" name="id" value="<?php echo 1; ?>">
+     
       <p align="right">
         <button type="Create">More Info</button>
       <br />

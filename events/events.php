a008a0b13f165c7b34c86802a62c6d73f5033b1e
ykien <ykien@iu.edu>
2022-03-31 16:56:40 -0400

    test image isplay

--- events/events.php
+++ events/events.php
@@ -147,7 +147,7 @@
 
         <?php while ($data = mysqli_fetch_assoc($select)) {?>
 		
-      <?php echo "<img src='". $data['image'] ."' />"; ?>
+      <?php echo "<img src='". $data['image'] ."'"; ?>
       <div>
 			  <label><b>Event Name:</b> <?php echo $data['event_name']; ?> </label>
 			<!-- <input type="text" placeholder="Culture Show" /> -->

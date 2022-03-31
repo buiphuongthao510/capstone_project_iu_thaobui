5a8bfd798f734b7fd2d5bb610f290e46fc170090
ykien <ykien@iu.edu>
2022-03-31 16:47:26 -0400

    test image display

--- events/events.php
+++ events/events.php
@@ -145,10 +145,12 @@
 	<form action="reserveEvent.php" method="GET">
       <div class="up-item b-line">
 
-        <?php while ($data = mysqli_fetch_assoc($select)) {?>
+        <?php while ($data = mysqli_fetch_assoc($select)) {
+         echo "<img src='" . $data['image'] . "' /> <br />";
+          ?>
 		
       <div>
-      <?php echo "<img src='". $data['image'] ."' />"; ?>
+  
 			  <label><b>Event Name:</b> <?php echo $data['event_name']; ?> </label>
 			<!-- <input type="text" placeholder="Culture Show" /> -->
 			<br />

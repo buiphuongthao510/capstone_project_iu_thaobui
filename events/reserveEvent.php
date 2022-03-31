dededa2170651d7781df475c3fe00ed4694fa39d
ykien <ykien@iu.edu>
2022-03-30 23:50:00 -0400

    test data display

--- events/reserveEvent.php
+++ events/reserveEvent.php
@@ -51,7 +51,7 @@
             if ($conn->connect_error) {
                 die("Connection failed: " .$conn->connect_error);
             }
-            session_start();
+    
 			
 			      $id = $_GET["id"];
 

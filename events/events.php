ee36d2c75554e43320e98faf0411d661b22d10dc
ykien <ykien@iu.edu>
2022-04-05 15:47:35 -0400

    test image display

--- events/events.php
+++ events/events.php
@@ -159,7 +159,7 @@
 		
       
       <div>
-      <?php echo "<img src='uploads/".$data['image']."'"; ?>
+      <?php echo "<img src='img/".$data['image']."'"; ?>
 			  <label><b>Event Name:</b> <?php echo $data['event_name']; ?> </label>
 			<!-- <input type="text" placeholder="Culture Show" /> -->
 			<br />

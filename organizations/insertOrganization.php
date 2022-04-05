ce1ef64952f9aa16c04aeb2fa0845851ceceaebf
Z01Y <zhang.ziyue99@gmail.com>
2022-04-05 17:30:29 -0400

    update insert organization

--- organizations/insertOrganization.php
+++ organizations/insertOrganization.php
@@ -21,12 +21,16 @@
    			die("Connection failed: " .$conn->connect_error);
 		}
 
+	// Get User Ticket
+		$cas_username = $_SESSION["username"];
+
 	// Taking values from form data 
 		$name = $_REQUEST['name'];
 		$email = $_REQUEST['email'];
 		$phone = $_REQUEST['phone'];
 		$bio = $_REQUEST['bio'];
         
+		
 
 	// Insert Query 
 		$sql = "INSERT INTO organizations(name,email,phone,bio) VALUES ('".$name."','".$email."','".$phone."','".$bio."')";
@@ -38,6 +42,16 @@
 			echo "ERROR: Hush! Sorry $sql. "
 			. mysqli_error($conn);
 		}
+
+		$sql_members = "INSERT INTO members (created_organizations) VALUES ('".$name."') 
+		if(mysqli_query($conn, $sql)){
+			echo "Data updated successfully";
+			echo nl2br("\n$name");
+		}	else{
+			echo "ERROR: Hush! Sorry $sql. "
+			. mysqli_error($conn);
+		}
+
 		
 		// Close Connection 
 		mysqli_close($conn);

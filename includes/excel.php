35b599808485f97cd1c0c94a3511e021f49310fb
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-06 18:02:08 -0400

    update excel.php

--- includes/excel.php
+++ includes/excel.php
@@ -7,7 +7,10 @@ header('Content-type: text/html; charset=utf-8');
 header("Content-type:application/vnd.ms-excel");
 //set Excel filename
 header("Content-Disposition:filename=PHP_est.xls");
-
+$servername = "db.luddy.indiana.edu";
+$username = "i494f21_team21";
+$password = "my+sql=i494f21_team21";
+$dbname = "i494f21_team21";
 $conn = mysqli_connect("db.luddy.indiana.edu", "i494f21_team21", "my+sql=i494f21_team21", "i494f21_team21");
 //determine data work or not
 if (mysqli_connect_error($conn)) {
@@ -17,6 +20,7 @@ die("connection failed :".mysqli_connect_error());
 //get data from sql database, set code 
 mysqli_query($conn, "SET NAMES utf8");
 
+// search from member database
 $sql = "SELECT first_name,last_name,email FROM members";
 //$id = $_GET['id'];
 //$sql = "SELECT first_name,last_name,email FROM members WHERE id=$id";
@@ -33,14 +37,23 @@ echo $name."\t";
 echo "\n";
 
 if ($result->num_rows > 0) {
-//related sets of data
-while($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
-// save data
-// $dataArr[] = $row;
-echo $row[0]."\t".$row[1]."\t".$row[2]."\t".$row[3]."\t\n";
-}
-}else{
-echo "No query data!";
-}
-
-$conn->close();
+    //related sets of data
+    while($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
+    // save data
+        $events_id = $row[3];
+    
+        $events_sql = "SELECT * FROM events WHERE id=$events_id";
+        $events_result = $conn->query($events_sql);//execute the sql
+        $events_data = $events_result->fetch_assoc();//event's data
+    //    var_dump($events_data);exit;
+    
+    // $dataArr[] = $row;
+        //export data
+    echo $row[0]."\t".$row[1]."\t".$row[2]."\t".$events_data['username']."\t".$events_data['event_time']."\t".$events_data['event_date']."\t".$events_data['address']."\t".$events_data['state_address']."\t\n";
+    }
+    
+    }else{
+    echo "No query data!";
+    }
+    
+    $conn->close();

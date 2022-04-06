5d17d332fd7658f5248134e89056cf9c631e3532
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-05 23:18:41 -0400

    update search feature

--- includes/dosearch.php
+++ includes/dosearch.php
@@ -1,10 +1,14 @@
 <?php
 
 // CONNECT DATABASE
-$servername = "db.luddy.indiana.edu";
-$username = "i494f21_team21";
-$password = "my+sql=i494f21_team21";
-$dbname = "i494f21_team21"; 
+// $servername = "db.luddy.indiana.edu";
+// $username = "i494f21_team21";
+// $password = "my+sql=i494f21_team21";
+// $dbname = "i494f21_team21"; 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "10721-500";
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " .$conn->connect_error);

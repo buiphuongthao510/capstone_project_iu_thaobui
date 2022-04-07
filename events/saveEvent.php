27f1059cb465b8c84601388b723793ee4246c29c
ykien <ykien@iu.edu>
2022-04-07 01:14:35 -0400

    test data display

--- events/saveEvent.php
+++ events/saveEvent.php
@@ -62,7 +62,7 @@
 			      $id = $_SESSION["id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address,description FROM events WHERE id = 1 ;";
+			$sql_select = "SELECT * FROM events;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

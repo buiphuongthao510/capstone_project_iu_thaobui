c0efc941d9d3a8880348d2f94fb247473999add0
ykien <ykien@iu.edu>
2022-04-07 01:32:04 -0400

    test break line

--- events/editEvent.php
+++ events/editEvent.php
@@ -109,10 +109,13 @@
       </div>
       <p align="left">
       <button> <? echo "<a href='saveEvent.php?id=$id'<button type='button' >Edit Event Information</button></a>"; ?> </button>
+      <br />
+      <br />
+      <br />
       
         <?php }?>
-        
-      <hr>
+
+      
 
       </div>
       </div>

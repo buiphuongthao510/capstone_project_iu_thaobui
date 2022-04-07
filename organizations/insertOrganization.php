d75f654d849eb37f651f214e09c42e4c458bc3e3
Z01Y <zhang.ziyue99@gmail.com>
2022-04-07 19:06:11 -0400

    test insert username

--- organizations/insertOrganization.php
+++ organizations/insertOrganization.php
@@ -1,3 +1,31 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+      echo '<script type="text/javascript">';
+      echo 'alert("Unauthorized page! Please login first!");';
+      echo 'window.location.href = "https://cgi.luddy.indiana.edu/~team21/index/login.php";';
+      echo '</script>';
+    }
+    else{
+            $servername = "db.luddy.indiana.edu";
+            $username = "i494f21_team21";
+            $password = "my+sql=i494f21_team21";
+            $dbname = "i494f21_team21";
+            
+            // Create connection
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+            }
+            
+            $cas_username = $_SESSION["username"];
+          }
+?>
+
+
+
 <!DOCTYPE html>
 <html>
   
@@ -8,6 +36,7 @@
 <body>
     <center>
     <?php
+
     //connect to Database 
 		$servername = "db.luddy.indiana.edu";
 		$username = "i494f21_team21";
@@ -21,9 +50,7 @@
    			die("Connection failed: " .$conn->connect_error);
 		}
 
-	// Get User Ticket
-		session_start();
-		$cas_username = $_SESSION["username"];
+
 
 	// Taking values from form data 
 		$name = $_REQUEST['name'];

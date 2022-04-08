51f1683984c4acce463f163555624518371543e7
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-08 02:16:16 -0400

    update navbar

--- events/editEvent.php
+++ events/editEvent.php
@@ -105,7 +105,7 @@
       <br /> 
       <p align="right">
       <a href="saveEvent.php"><button>Edit Event Information</button></a>
-        <a href="https://cgi.luddy.indiana.edu/~team21/includes/excel.php" target="_blank"><button>excel66</button></a>
+        <a href="https://cgi.luddy.indiana.edu/~team21/includes/excel.php" target="_blank"><button>excel</button></a>
  <?php }?>
 
       </fieldset>
--- events/events.php
+++ events/events.php
@@ -30,15 +30,15 @@
 <body>
   <div style="width:1280px;margin: 0 auto;">
 
-    <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
       <div class="menu-list">
+    <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
         <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
         <a href="">Donation</a>
         <a href="#"><?php include_once("../includes/search.php")?></a>
-        
+        <div class="ui"><a href="https://cgi.luddy.indiana.edu/~team21/index/profile.php">UI</a></div>
       </div>
-    <div class="ui"><a href="https://cgi.luddy.indiana.edu/~team21/index/profile.php">UI</a></div>
+    <!-- <div class="ui"><a href="https://cgi.luddy.indiana.edu/~team21/index/profile.php">UI</a></div> -->
 </div>
  
 

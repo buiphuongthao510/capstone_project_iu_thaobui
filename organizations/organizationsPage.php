c8e95a68aafd41a2a06730e3032da9c65d6e4542
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-08 00:59:52 -0400

    update navbar

--- organizations/organizationsPage.php
+++ organizations/organizationsPage.php
@@ -13,7 +13,7 @@
 <?php include '../includes/header.php'; ?>
 
 <body>
-<header style=“width:1280px;margin: 0 auto”>
+<header style=“width:1280px;margin: 0 auto;”>
     <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
     <div class="menu-list">
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>

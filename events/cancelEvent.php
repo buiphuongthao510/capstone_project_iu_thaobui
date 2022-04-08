1da0a245e904b02e63b2a3fc8c5766301dca3fb4
buithao <thaobui@iu.edu>
2022-04-08 13:48:24 -0400

    cancel events button

---
+++ events/cancelEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['username'])){
+    echo '<script type="text/javascript">';
+    echo 'alert("You need to login in order to register for the event.");';
+    echo '</script>';
+} else {
+    // CONNECT DATABASE
+    $servername = "db.luddy.indiana.edu";
+    $username = "i494f21_team21";
+    $password = "my+sql=i494f21_team21";
+    $dbname = "i494f21_team21";
+
+    // Create connection
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " .$conn->connect_error);
+    }
+
+    $cas_username = $_SESSION["username"];
+    
+    $sql_update = "UPDATE members SET e_id = null WHERE username = '".$cas_username."';";
+
+    if ($conn->query($sql_update) === TRUE) {
+        // echo "username: " .$cas_username. "<br>";
+        // echo "id: " .$id. "<br>";
+      } else {
+        echo "Error: " .$sql_update. "<br>".$conn->error;
+      }
+}
+
+?>

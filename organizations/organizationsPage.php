2e409266f6078674773a3ee4f0716f17fcb7d445
buithao <thaobui@iu.edu>
2022-04-08 11:59:47 -0400

    fix header for organization page

--- organizations/organizationsPage.php
+++ organizations/organizationsPage.php
@@ -13,17 +13,20 @@
 <?php include '../includes/header.php'; ?>
 
 <body>
-<header style=“width:1280px;margin: 0 auto;”>
-    <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
-    <div class="menu-list">
+<header>
+      <div class="logo"><a href="https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a></div>
+      <div class="menu-list">
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
         <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
         <a href="">Donation</a>
         <a href="#"><?php include_once("../includes/search.php")?></a>
-    </div>
-    <div class="ui"><a href="https://cgi.luddy.indiana.edu/~team21/index/profile.php">UI</a></div>
-
-</header>
+      </div>
+      <div class="button-wrap">
+        <div class="button login">
+          <a href="profile.php"><button>User Profile</button></a>
+        </div>
+      </div>
+  </header>
 
 
 <div class="main">

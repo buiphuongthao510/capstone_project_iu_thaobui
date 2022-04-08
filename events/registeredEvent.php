f01d4f0a2911e191f18923539f20daf60f25c849
ykien <ykien@iu.edu>
2022-04-08 12:20:52 -0400

    try data display using 2 tables

--- events/registeredEvent.php
+++ events/registeredEvent.php
@@ -70,7 +70,7 @@
 			      $id = $_SESSION["id"];
 			
 			//select statements
-			$sql_select = "SELECT event_name,event_date,event_time,address,description FROM events WHERE id = 1 ;";
+			$sql_select = "SELECT events.event_name, events.event_date, events.event_time, events.address, events.description, members.e_id FROM events, members WHERE events.id = members.e_id ;";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

509a4a4945ac61a3c9b577bff9d3c3d14d7ecc46
ykien <ykien@iu.edu>
2022-04-09 19:47:55 -0400

    test img storage

--- events/insertEvent.php
+++ events/insertEvent.php
@@ -57,7 +57,7 @@
 
 		$path = $image['name'];
 
-		$path = "events/img/" . $path;
+		$path = "C:/Users/13175/ykien/team-21/events/img" . $path;
 		move_uploaded_file($image['tmp-name'], $path);
 		
 	// Insert Query 

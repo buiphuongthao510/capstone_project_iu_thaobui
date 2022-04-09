a12a77f177007c809d9ed777f4788c9188c57202
ykien <ykien@iu.edu>
2022-04-09 06:20:03 -0400

    orginal code

--- events/reserveEvent.php
+++ events/reserveEvent.php
@@ -57,13 +57,13 @@
             }
             session_start();
 			
-			      $username = $_GET["username"];
-            $_SESSION['username'] = $username;
+			      $username = $_GET["id"];
+            $_SESSION['id'] = $id;
 
 			
 			//select statements
       
-			$sql_select = "SELECT * FROM events";
+			$sql_select = "SELECT event_name,event_date,event_time,address,description FROM events WHERE id = $id";
 			
 			$select = mysqli_query($conn, $sql_select);
 		

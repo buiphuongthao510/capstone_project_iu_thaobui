925d3e876bea9d795971335da669f3014d4667b4
ykien <ykien@iu.edu>
2022-04-09 03:03:26 -0400

    test edit

--- events/editEvent.php
+++ events/editEvent.php
@@ -1,3 +1,29 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+      echo '<script type="text/javascript">';
+      echo 'alert("Unauthorized page! Please login first!");';
+      echo 'window.location.href = "https://cgi.luddy.indiana.edu/~team21/index/login.php";';
+      echo '</script>';
+    }
+    else{
+            $servername = "db.luddy.indiana.edu";
+            $username = "i494f21_team21";
+            $password = "my+sql=i494f21_team21";
+            $dbname = "i494f21_team21";
+            
+            // Create connection
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+            }
+            
+            $cas_username = $_SESSION["username"];
+          }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -68,13 +94,10 @@
             if ($conn->connect_error) {
                 die("Connection failed: " .$conn->connect_error);
             }
-            session_start();
-
-            $cas_username = $_SESSION["username"];
-			      $id = $_SESSION["id"];
+            
 			
 			//select statements
-      $sql_select = "SELECT events.event_name, events.event_date, events.event_time, events.address, events.description FROM events, members WHERE events.id = members.e_id AND members.username = '".$cas_username."';";			
+      $sql_select = "SELECT events.event_name, events.event_date, events.event_time, events.address, events.description FROM events, members WHERE username = '".$cas_username."';";			
 			
       $select = mysqli_query($conn, $sql_select);
 		

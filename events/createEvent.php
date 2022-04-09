44c607198357a5434a8e01d9f5281f8891e61920
ykien <ykien@iu.edu>
2022-04-09 02:36:16 -0400

    test create event using username

--- events/createEvent.php
+++ events/createEvent.php
@@ -1,3 +1,29 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+      echo '<script type="text/javascript">';
+      echo 'alert("Unauthorized page! Please login first!");';
+      echo 'window.location.href = "https://cgi.luddy.indiana.edu/~team21/index/login.php";';
+      echo '</script>';
+    }
+    else{
+            $servername = "db.luddy.indiana.edu";
+            $username = "i494f21_team21";
+            $password = "my+sql=i494f21_team21";
+            $dbname = "i494f21_team21";
+            
+            // Create connection
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+            }
+            
+            $cas_username = $_SESSION["username"];
+          }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -60,7 +86,7 @@
 				<br />
 				<label>Event Description <br /> <textarea id="description" name="description" rows="10" cols="50"></textarea></label>
         <p align="right">
-        <input type="submit" value="Create Event" />
+        <button type="submit" name="submit">Create Event</button>
 			</form>
 			
 			</div>

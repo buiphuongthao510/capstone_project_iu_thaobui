15927fe042501fa8cdc1ec7238cf635f79134145
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-10 23:33:39 -0400

    update search function

--- includes/search.php
+++ includes/search.php
@@ -29,10 +29,11 @@
             //data for returning
             success: function (data) {
 
-             //search out data using jquery to work ajax and turn on website 
-             $.each(data, function(i, n){
+            //search out data using jquery to work ajax and turn on website 
+            $.each(data, function(i, n){
                if(n.event_name !== ''){
-                        $(".content").prepend('<br>< a href="https://cgi.luddy.indiana.edu/~team21/includes/search_info.php?o_id='+n.o_id+'" style="color: #FFFFFF;"><event></event>event name: '+n.event_name+'</ a><br><br>')
+                        $(".content").prepend('<br>< a href="https://cgi.luddy.indiana.edu/~team21/includes/search_info.php?o_id='+n.o_id+'" style="color: #FFFFFF;"><event></event> event name: '+n.nevent_nameame+'</ a><br><br>')
+
                     }else{
                         $(".content").prepend('<br>< a href="https://cgi.luddy.indiana.edu/~team21/includes/search_info.php?o_id='+n.o_id+'" style="color: #FFFFFF;"><event></event> name: '+n.name+'</ a><br><br>')
                     }

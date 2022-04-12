03f9fb88c7753030afc81837a6692919e0eae5d8
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-11 21:51:19 -0400

    update excel.php

--- includes/excel.php
+++ includes/excel.php
@@ -2,11 +2,11 @@
 
 //must have, for export excel;
 //set code
-header('Content-type: text/html; charset=utf-8');
-//set Excel
-header("Content-type:application/vnd.ms-excel");
-//set Excel filename
-header("Content-Disposition:filename=PHP_est.xls");
+// header('Content-type: text/html; charset=utf-8');
+// //set Excel
+// header("Content-type:application/vnd.ms-excel");
+// //set Excel filename
+// header("Content-Disposition:filename=PHP_est.xls");
 $servername = "db.luddy.indiana.edu";
 $username = "i494f21_team21";
 $password = "my+sql=i494f21_team21";
@@ -40,17 +40,7 @@ echo "\n";
 if ($result->num_rows > 0) {
     //related sets of data
     while($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
-    // save data
-        // $events_id = $row[3];
-    
-        // $events_sql = "SELECT * FROM events WHERE id=$events_id";
-        // $events_result = $conn->query($events_sql);//execute the sql
-        // $events_data = $events_result->fetch_assoc();//event's data
-    //    var_dump($events_data);exit;
-    
-    // $dataArr[] = $row;
-        //export data
-    // echo $row[0]."\t".$row[1]."\t".$row[2]."\t".$events_data['username']."\t".$events_data['event_time']."\t".$events_data['event_date']."\t".$events_data['address']."\t".$events_data['state_address']."\t\n";
+var_dump($row);exit;
 
     echo $row[0]."\t".$row[1]."\t".$row[2]."\t"."\t\n";
     }

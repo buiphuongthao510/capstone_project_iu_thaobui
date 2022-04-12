fe518a52f97b8a5e6cd838602fd281a68becacd7
Chen, Yen-Chun <chenyenc@iu.edu>
2022-04-11 22:11:09 -0400

    update editEvent.php

--- events/editEvent.php
+++ events/editEvent.php
@@ -97,7 +97,7 @@
             
 			
 			//select statements
-      $sql_select = "SELECT event_name, event_date, event_time, address, description FROM events WHERE username = '".$cas_username."';";			
+      $sql_select = "SELECT id,event_name, event_date, event_time, address, description FROM events WHERE username = '".$cas_username."';";			
 			
       $select = mysqli_query($conn, $sql_select);
 		

e0dd530b5b34431b6397df0a03b2eb4cd42a2278
Z01Y <zhang.ziyue99@gmail.com>
2022-04-14 17:10:36 -0400

    update report

---
+++ fraud report/report.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+      echo '<script type="text/javascript">';
+      echo 'alert("Unauthorized page! Please login first!");';
+      echo 'window.location.href = "https://cgi.luddy.indiana.edu/~team21/index/login.php";';
+      echo '</script>';
+    }
+    else{
+            $servername = "db.luddy.indiana.edu";
+            $username = "i494f21_team21";
+            $password = "my+sql=i494f21_team21";
+            $dbname = "i494f21_team21";
+            
+            // Create connection
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+            
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " .$conn->connect_error);
+            }
+            
+            $cas_username = $_SESSION["username"];
+          }
+?>

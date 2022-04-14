5e8769566b119afa42472e48d89cb1097d932026
Z01Y <zhang.ziyue99@gmail.com>
2022-04-14 18:49:33 -0400

    update header

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
       <div class="menu-list">
         <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
         <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
-        <a href="">Donation</a>
+        <a href="https://cgi.luddy.indiana.edu/~team21/report/report.php">Report</a>
         <a href="">Search</a>
       </div>
       <div class="button-wrap">

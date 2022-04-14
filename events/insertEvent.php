ac37c0ef6a11afd76758bcc7078211f9002c82b2
ykien <ykien@iu.edu>
2022-04-14 19:20:20 -0400

    test update

--- events/insertEvent.php
+++ events/insertEvent.php
@@ -63,7 +63,7 @@
 		
 	// Insert Query 
 		$sql = "INSERT INTO events(event_name,event_date,event_time,address,description,image, username) VALUES ('".$event_name."','".$event_date."','".$event_time."','".$address."','".$description."','".$path."','".$cas_username."');";
-		$update_sql = "UPDATE events SET events.o_id = (SELECT organizations.o_id FROM organizations WHERE organizations.m_username = events.username);";
+		$update_sql = "UPDATE events SET events.o_id = (SELECT organizations.o_id FROM organizations WHERE organizations.m_username = events.username OR events.username = null);";
 
 		if(mysqli_query($conn, $sql)){
 			echo "Data updated successfully";

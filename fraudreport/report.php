09be4bb1393b90f3cd8725b027e1e920035c0604
Z01Y <zhang.ziyue99@gmail.com>
2022-04-14 17:14:18 -0400

    test report page

--- fraudreport/report.php
+++ fraudreport/report.php
@@ -23,3 +23,28 @@
             $cas_username = $_SESSION["username"];
           }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="./createOrg.css" rel="stylesheet" />
+    <title>Create Org</title>
+  </head>
+
+  <body>
+  <header>
+      <div class="logo">
+        <a href = "https://cgi.luddy.indiana.edu/~team21/index/index.php">Youthon</a>
+      </div>
+      <div class="menu-list">
+        <a href="https://cgi.luddy.indiana.edu/~team21/events/events.php">Events</a>
+        <a href="https://cgi.luddy.indiana.edu/~team21/organizations/organizationsPage.php">Organizations</a>
+        <a href="">Fraud Report</a>
+        <a href="#"><?php include_once("../includes/search.php")?></a>
+      </div>
+    </header>
+
+  

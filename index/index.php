91f8fbd9cf9d46b3658fcbf2ad332703a76c406e
buithao <thaobui@iu.edu>
2022-04-15 13:49:03 -0400

    turn leaderboard into table tag test

--- index/index.php
+++ index/index.php
@@ -318,7 +318,7 @@
             while($rows = mysqli_fetch_array($result)){
           
           ?>
-          <tr>
+          <tr class="row">
             <?php echo '<td class="order order-1">'.$ranking.'</td>'?>
             <?php echo '<td class="name">'.$rows['name'].'</td>'?>
             <?php echo '<td>'.$rows['points'].'</td>'?>
